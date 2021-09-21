fa378842e74b1f445672561649b8b3e5e2b79d1c
shellytsukerman <shellytsukerman@SHELLYTSUKEC4D9>
2021-09-21 16:54:25 +0300

    Add project files.

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("15");
+            Console.WriteLine(5 * 6);
+            Console.WriteLine();
+            Console.WriteLine("My name is Shelly");
+            Console.WriteLine("");
+        }
+    }
+}
